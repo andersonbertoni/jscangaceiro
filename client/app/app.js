f23b8a460cfa8f2fd4825230223bf981c7f8d90f
Anderson Cassio Bertoni <andersonbertoni@gmail.com>
2018-12-06 22:28:48 -0200

    + código atualizado até o item 3.9 Arrows Functions: Deixando o código ainda menos verboso;

--- client/app/app.js
+++ client/app/app.js
@@ -6,4 +6,5 @@ let controller = new NegociacaoController();
 //associa o evento de submissão do formulário à chamada do método "adiciona"
 document
     .querySelector(".form")
-    .addEventListener("submit", controller.adiciona);
+    //bind aqui!
+    .addEventListener("submit", controller.adiciona.bind(controller));
--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,10 +1,35 @@
 // client/app/controllers/NegociacaoController.js
 
 class NegociacaoController {
+    constructor() {
+        //a ideia é que $ seja o querySelector
+        //realizando o bind, $ mantém document como seu contexto this
+        let $ = document.querySelector.bind(document);
+
+        //buscando os elementos
+        //usaremos o prefixo _ (a convenção para propriedades privadas)
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
     adiciona(event) {
         //cancelando a submissão do formulário
-        event.preventDefault();
+        event.preventDefault(); 
 
-        alert("Chamei ação no controller");
+        //lembre-se de que arrays começam com índice 0, 
+        //logo, 1 é o segundo elemento!
+        let data = new Date(...this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2 ));
+
+        //precisamos acessar as propriedades através de this
+        let negociacao = new Negociacao(        
+            data,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        console.log(negociacao);
     }
 }
